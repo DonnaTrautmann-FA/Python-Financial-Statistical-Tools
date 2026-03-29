1f90172b2deb7af101c1cda0b333b8098e317cc2
DonnaTrautmann-FA <coordinatepoint@yahoo.com>
2026-03-28 19:41:27 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import mean_var_std
+from unittest import main
+
+# This tests your function with a sample list
+print(mean_var_std.calculate([0,1,2,3,4,5,6,7,8]))
+
+# This runs the full suite of automated tests
+main(module='test_module', exit=False)
